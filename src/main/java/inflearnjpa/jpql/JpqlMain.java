822dc5756d80c3b504dd44af1e3fda855a5e6d4d
EUIJOON <euijoonism@gmail.com>
2023-08-24 13:34:04 +0900

    섹션 10. 객체지향 쿼리 언어1 - 심화 문법 - 페치 조인 (1)

--- src/main/java/inflearnjpa/jpql/JpqlMain.java
+++ src/main/java/inflearnjpa/jpql/JpqlMain.java
@@ -16,26 +16,45 @@ public class JpqlMain {
     tx.begin();
 
     try {
+      Team teamA = new Team();
+      teamA.setName("팀A");
+      em.persist(teamA);
+
+      Team teamB = new Team();
+      teamB.setName("팀B");
+      em.persist(teamB);
 
       Member member1 = new Member();
-      member1.setUsername("관리자 1");
+      member1.setUsername("회원1");
+      member1.setTeam(teamA);
       em.persist(member1);
 
       Member member2 = new Member();
-      member2.setUsername("관리자 2");
+      member2.setUsername("회원2");
+      member2.setTeam(teamA);
       em.persist(member2);
+
+      Member member3 = new Member();
+      member3.setUsername("회원3");
+      member3.setTeam(teamB);
+      em.persist(member3);
 
 
       em.flush();
       em.clear(); // 비움
 
 
-      String query = "select function('group_concat', m.username) from Member m";
-      List<String> result = em.createQuery(query, String.class)
+//      String query = "select m from Member m";
+      String query = "select m from Member m join fetch m.team";
+      List<Member> result = em.createQuery(query, Member.class)
           .getResultList();
 
-      for (String s : result) {
-        System.out.println("s = " + s);
+      for (Member member : result) {
+        System.out.println("member = " + member + ", " + member.getTeam().getName());
+        //회원1, 팀A(SQL)
+        //회원2, 팀A(1차캐시 => 쿼리 없이 바로가져옴)
+
+        //회원3 ,팀B(1차캐시에 없으므로 새로운 쿼리, SQL)
       }
 
       tx.commit();
